c21afc3c4d17a3d706d043f84527b2027614f6f0
Amin <a_ayoub@hotmail.co.uk>
2020-03-09 10:48:29 +0000

    Create ElevatorConstants.java

---
+++ Practice-Programs/ElevatorRefined/ElevatorConstants.java
@@ -0,0 +1,10 @@
+package com.ynap.assessment;
+
+public class ElevatorConstants {
+	
+	public static final String ELEVATOR_UPWARD_DIRECTION = "UP";
+	public static final String ELEVATOR_DOWNWARD_DIRECTION = "DOWN";
+	public static final Integer TOP_FLOOR = 12;
+	public static final Integer LOWEST_FLOOR = 0;
+
+}
