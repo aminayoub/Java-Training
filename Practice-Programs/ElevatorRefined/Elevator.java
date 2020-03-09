2ba112df6da600c9daad949d048262be39511b35
Amin <a_ayoub@hotmail.co.uk>
2020-03-09 10:48:06 +0000

    Create Elevator.java

---
+++ Practice-Programs/ElevatorRefined/Elevator.java
@@ -0,0 +1,18 @@
+package com.ynap.assessment;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.Set;
+import com.ynap.assessment.FloorComparator;
+
+public class Elevator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		TestElevatorRun run = new TestElevatorRun(input);
+		System.out.println(run.init());   
+	}
+}
